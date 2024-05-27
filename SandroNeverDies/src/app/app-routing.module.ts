1340e71d727def8df85fa8b6620ced3bfeb4a96a
MarceloPacheco16 <marcelop639@gmail.com>
2024-05-27 03:25:04 -0300

    Primer Version en Angular
    Pasar la pagina web a un proyecto Angular reorganizando las pantallas principales en componentes, y modificando las rutas (imágenes) y el código (iconos de Inicio: html - css)

---
+++ SandroNeverDies/src/app/app-routing.module.ts
@@ -0,0 +1,56 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NavigationComponent } from "./components/navigation/navigation.component";
+import { LoginComponent } from "./components/login/login.component";
+import { UsuarioRegistroComponent } from "./components/usuario-registro/usuario-registro.component";
+import { InicioComponent } from "./components/inicio/inicio.component";
+import { ShopComponent } from "./components/shop/shop.component";
+import { DetailComponent } from "./components/detail/detail.component";
+import { CartComponent } from "./components/cart/cart.component";
+import { CheckoutComponent } from "./components/checkout/checkout.component";
+import { ContactComponent } from "./components/contact/contact.component";
+// ContactComponent
+const routes: Routes = [
+	{
+		path: 'usuarios/navigation',
+		component: NavigationComponent
+	},
+	{
+		path: 'usuarios/login',
+		component: LoginComponent
+	},
+	{
+		path: 'usuarios/usuario-registro',
+		component: UsuarioRegistroComponent
+	},
+	{
+		path: 'usuarios/inicio',
+		component: InicioComponent
+	},
+	{
+		path: 'productos/shop',
+		component: ShopComponent
+	},
+	{
+		path: 'productos/detail',
+		component: DetailComponent
+	},
+	{
+		path: 'productos/cart',
+		component: CartComponent
+	},
+	{
+		path: 'productos/checkout',
+		component: CheckoutComponent
+	},
+	{
+		path: 'usuarios/contact',
+		component: ContactComponent
+	},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ SandroNeverDies/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { LoginComponent } from './components/login/login.component';
+import { UsuarioRegistroComponent } from './components/usuario-registro/usuario-registro.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ShopComponent } from './components/shop/shop.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { CartComponent } from './components/cart/cart.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { ContactComponent } from './components/contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    LoginComponent,
+    UsuarioRegistroComponent,
+    InicioComponent,
+    ShopComponent,
+    DetailComponent,
+    CartComponent,
+    CheckoutComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
